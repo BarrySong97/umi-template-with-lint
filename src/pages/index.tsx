a56e1ec585789022f1056868e0fd193f83dffb00
Barry Song <524000659@qq.com>
2021-07-13 17:23:06 +0800

    234

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
 import styles from './index.less';
-
 export default function IndexPage() {
-  const name = "123";
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
